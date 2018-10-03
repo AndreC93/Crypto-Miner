d548f063279db05c65a1edd57d477d5253ea70c4
Andre <fendred@yahoo.com>
2018-10-02 20:44:14 -0400

    Refactor tto classes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./index.js",
+    entry: "./lib/index.js",
     output: {
         path: path.join(__dirname, "lib"),
         filename: "bundle.js",
